c6c29486591059f38e7d9b690edc3baec87695fe
Gennady <g.korolew@gmail.com>
2025-02-06 21:45:17 +0400

    внесение исправлений в д/з «Миграции*» v1

--- src/main/resources/db/migration/V2__insert_initial_data.sql
+++ src/main/resources/db/migration/V2__insert_initial_data.sql
@@ -1,6 +1,6 @@
 INSERT INTO joins_homework.CUSTOMERS (name, surname, age, phone_number)
 VALUES
-('Alexey', 'Ivanov', 30, '1234567890'),
+('Ivan', 'Ivanov', 30, '1234567890'),
 ('John', 'Smith', 25, '9876543210'),
 ('Maria', 'Lopez', 35, '5558887777'),
 ('Sophia', 'Brown', 28, '4445556666');
